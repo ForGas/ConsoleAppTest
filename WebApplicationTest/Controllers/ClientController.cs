c8c1e9b619476bfcfcfe311c0fee3b1cd528600c
ForGas <lieber_kz@mail.ru>
2021-06-29 12:38:34 +0600

    feat add client conrtoller implementation

--- WebApplicationTest/Controllers/ClientController.cs
+++ WebApplicationTest/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleAppTest.Data;
+using ConsoleAppTest.Data.Model;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -16,11 +17,60 @@ namespace WebApplicationTest.Controllers
             _repository = repository;
         }
 
+        // GET: ClientController
+        [HttpGet]
         public IActionResult Index()
         {
             var clients = _repository.GetAll().ToList();
 
             return View(clients);
+        }
+
+        // POST: ClientController/Remove/5
+        [HttpGet]
+        public JsonResult Remove(long Id)
+        {
+            var client = _repository.Get(Id);
+
+            if (client == null)
+            {
+                return Json(false);
+            }
+            _repository.Remove(client);
+
+            return Json(true);
+        }
+
+        // GET: ClientController/View/5
+        [HttpGet]
+        public IActionResult View(long Id)
+        {
+            var client = _repository.Get(Id);
+
+            return View(client);
+        }
+
+        // GET: ClientController/Edit/5
+        [HttpGet]
+        public IActionResult Edit(long Id)
+        {
+            var client = _repository.Get(Id);
+
+            return View(client);
+        }
+
+        // POST: ClientController/Edit/5
+        [HttpPost]
+        public IActionResult Edit(BankClient client)
+        {
+            if (client is null)
+            {
+                return BadRequest(client);
+            }
+
+            _repository.Save(client);
+
+            return View(client);
         }
     }
 }
---
+++ WebApplicationTest/wwwroot/js/client.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    console.log('gg');
+    $('.delete').click(function () {
+        var clicked = $(this);
+        clicked.attr('disabled', 'disabled');
+
+        console.log('gg');
+        var id = clicked.attr('client-id');
+        var url = '/Client/Remove?Id=' + id;
+
+        $.get(url).done(function (answer) {
+            if (answer) {
+                clicked.closest(`#${id}`).remove();
+            }
+        });
+    });
+});
+
